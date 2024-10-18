1b352ff0d06964cc0021aec76561281d54bf21d8
AndersonN031 <devandersonnunes078@gmail.com>
2024-10-18 10:28:54 -0300

    teste

--- api/index.ts
+++ api/index.ts
@@ -3,22 +3,20 @@ import { PORT } from '../src/secrets';
 import rootRouter from '../src/routes';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
-import pool from '../src/lib/database'
+import pool from '../src/lib/database'; // A conexão com o banco de dados
 
 const app: Express = express();
 app.use(cors());
-
-app.use(express.json())
-
-app.use('/api', rootRouter)
-
+app.use(express.json());
+app.use('/api', rootRouter);
 
 export const prismaClient = new PrismaClient({
     log: ['query']
-})
-
-app.listen(PORT, () => {
-    console.log('Servidor rodando em http://localhost:3000/api/list/user');
-})
+});
 
 export { pool }
+
+// Exporta a função que será utilizada pela Vercel
+export default (req: Request, res: Response) => {
+    return app(req, res);
+};
