9e6191570e62f83196110280b26dc7157ae93309
AndersonN031 <devandersonnunes078@gmail.com>
2024-10-21 08:01:41 -0300

    save

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,38 @@
 import express, { Express, Request, Response } from 'express';
-// import { PORT } from '../src/secrets';
-import rootRouter from '../src/routes';
+import rootRouter from '../src/routes'; // Supondo que suas rotas estão configuradas aqui
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import pool from '../src/lib/database'; // A conexão com o banco de dados
+import path from 'path'; // Para manipulação de caminhos de arquivos
 
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api', rootRouter);
+app.use('/api', rootRouter); // Usando o router para rotas da API
 
+// Inicializa o cliente Prisma
 export const prismaClient = new PrismaClient({
     log: ['query']
 });
 
-export { pool }
+export { pool };
 
-// Exporta a função que será utilizada pela Vercel
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 export default (req: Request, res: Response) => {
     return app(req, res);
 };
 
-const PORT = 3000
-app.listen(PORT, () => {
-    console.log('Servidor rodando em http://localhost:3000/api/list/user')
-})
+// Configuração do servidor
+const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}/api/list/user`);
+    });
+}
